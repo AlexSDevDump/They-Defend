301dade508a53e69e42b608091d6c63823becf10
Alex Sampson <32361325+15015483Alex@users.noreply.github.com>
2020-01-16 15:54:11 +0000

    Wave Spawner V1

---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "New Wave", menuName = "Enemies/New Wave", order = 1)]
+public class Wave : ScriptableObject
+{
+    public List<EnemySO> enemyTypes = new List<EnemySO>();
+
+    public List<int> enemyQuantities = new List<int>();
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public GameObject enemyBase;
+    public float spawnGrace;
+
+    public Wave[] waves;
+    public int waveNumber;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(WaveLoop(waves[waveNumber]));
+            waveNumber++;
+        }
+    }
+
+    IEnumerator WaveLoop(Wave waveToSpawn)
+    {
+        for (int i = 0; i < waveToSpawn.enemyTypes.Count; i++)
+        {
+            for (int x = 0; x < waveToSpawn.enemyQuantities[i]; x++)
+            {
+                SpawnEnemy(waveToSpawn.enemyTypes[i]);
+                yield return new WaitForSeconds(spawnGrace);
+            }
+        }
+    }
+
+    private void SpawnEnemy(EnemySO eSO)
+    {
+        GameObject eToSpawn = Instantiate(enemyBase, transform.position, Quaternion.identity);
+        eToSpawn.GetComponent<EnemyController>().SO = eSO;
+    }
+}
